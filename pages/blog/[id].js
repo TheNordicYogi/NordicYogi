ed407cc9a26d51bd540db6d39a61e32a1ac5c0c4
TheNordicYogi <63834539+TheNordicYogi@users.noreply.github.com>
2021-02-09 20:55:32 -0700

    Header on all blog posts now spans width of content

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -23,34 +23,39 @@ export async function getStaticPaths() {
 
 export default function Post({ postData }) {
   return (
-    <Layout>
-      <Head>
-        <title>
-          {postData.title}: {postData.subtitle}
-        </title>
-      </Head>
-      <div className="max-w-xl h-auto">
-        <img
-          loading="eager"
-          className="rounded"
-          src={postData.image}
-          alt={postData.alt}
-        />
-      </div>
-      <article>
-        <h1 className="text-4xl tracking-tighter my-4 mx-0 font-extrabold">
-          {postData.title}:
-          <span className="text-gray-400"> {postData.subtitle}</span>
-        </h1>
-
-        <div className="text-gray-400">
-          <Date dateString={postData.date} />
+    <>
+      <div className="blog">
+        <div className="w-full md:max-w-xl h-auto mx-auto">
+          <img
+            loading="eager"
+            className="rounded"
+            src={postData.image}
+            alt={postData.alt}
+          />
         </div>
-        <div
-          className="mt-4"
-          dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
-        />
-      </article>
-    </Layout>
+        <Layout>
+          <Head>
+            <title>
+              {postData.title}: {postData.subtitle}
+            </title>
+          </Head>
+
+          <article>
+            <h1 className="text-4xl tracking-tighter my-4 mx-0 font-extrabold">
+              {postData.title}:
+              <span className="text-gray-400"> {postData.subtitle}</span>
+            </h1>
+
+            <div className="text-gray-400">
+              <Date dateString={postData.date} />
+            </div>
+            <div
+              className="mt-4"
+              dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+            />
+          </article>
+        </Layout>
+      </div>
+    </>
   );
 }
