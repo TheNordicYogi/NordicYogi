985c1f3c3ce9bad6744a431ccdae1048965b8bf8
TheNordicYogi <63834539+TheNordicYogi@users.noreply.github.com>
2021-02-10 18:03:31 -0700

    added service worker

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const withOffline = require("next-offline");
-
-// your next.js configs
-const nextConfig = {};
-
-module.exports = withOffline(nextConfig);
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,26 @@
 import "../styles/globals.css";
 import { TrackingProvider } from "../contexts/trackers";
-import Head from "next/head";
+import { useEffect, componentDidMount } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
+
   return (
     <TrackingProvider value="dark">
       <Component {...pageProps} />;
